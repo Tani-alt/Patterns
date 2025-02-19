2ab075c7150bccc928f4651841cd7009986cac73
Tani-alt <Tanirika.gupta@gmail.com>
2025-02-19 21:08:37 +0530

    mirrod triangle

--- Prroject.py
+++ Prroject.py
@@ -1,5 +1,7 @@
 #TRIANGLE
-for i in range(1,6):  
+n=int(input("enter the num of rows"))
+for i in range(n):
+     for j in range (i+1)  :
         left_triangle = "*" * i
         right_triangle = "*" * (6 - i)
         print(left_triangle + " " + right_triangle)
